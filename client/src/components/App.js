804f2f870eaae535a7fd2cb982a1c7689632fc45
brianpatrickhummel <frankgucci@mac.com>
2017-12-28 17:08:09 -0800

    added the Ant Design react UI library

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import Button from "antd/lib/button";
+import "antd/dist/antd.css";
 
 class App extends Component {
   render() {
-    return <h1>HomePage</h1>;
+    return (
+      <div>
+        <h1>Welcome/Login Screen</h1>
+        <Button type="primary">Button</Button>
+      </div>
+    );
   }
 }
 

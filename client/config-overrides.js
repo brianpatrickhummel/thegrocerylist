8582b580e815875c31a16e584af26657ec3c4510
brianpatrickhummel <frankgucci@mac.com>
2018-01-02 12:25:01 -0800

    installed the styled-components library. installed eact-app-rewire-less so that the Ant Design CSS theme can be customized using Less variables, configured in config-overrides.js

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,7 +1,12 @@
 const { injectBabelPlugin } = require("react-app-rewired");
+const rewireLess = require("react-app-rewire-less");
 
 module.exports = (config, env) => {
-  config = injectBabelPlugin(["import", { libraryName: "antd", libraryDirectory: "es", style: "css" }], config);
+  config = injectBabelPlugin(["import", { libraryName: "antd", style: true }], config);
+  // change importing css to less
+  config = rewireLess.withLoaderOptions({
+    modifyVars: { "@body-background": "#6c4c4c" }
+  })(config, env);
 
   return config;
 };

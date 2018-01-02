d3c1749997f05e912256356354aa83f41e0d3ead
brianpatrickhummel <frankgucci@mac.com>
2018-01-02 11:55:58 -0800

    react-app-rewired configures webpack to allow for the Ant Design css to load per imported component rather than than having to import the entire css sheet.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { Icon, Button, DatePicker } from "antd";
-const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
+import { Button, DatePicker } from "antd";
+const { RangePicker } = DatePicker;
 
 function onChange(date, dateString) {
   console.log(date, dateString);
@@ -11,18 +11,13 @@ class App extends Component {
     return (
       <div style={{ textAlign: "center" }}>
         <h1>Welcome & Login Page</h1>
-        <Icon type="step-backward" />
         <Button type="primary" ghost>
           <a href="/auth/google">LogIn With Google</a>
         </Button>
         <div>
-          <DatePicker onChange={onChange} />
-          <br />
-          <MonthPicker onChange={onChange} placeholder="Select month" />
           <br />
           <RangePicker onChange={onChange} />
           <br />
-          <WeekPicker onChange={onChange} placeholder="Select week" />
         </div>
       </div>
     );

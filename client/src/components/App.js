384ab590bca66f99a048f456c615f4b23e36e49f
brianpatrickhummel <frankgucci@mac.com>
2018-01-02 11:41:17 -0800

    successful implementation of react-app-reqired into create-react-app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
-import { Row, Col, Icon, Button } from "antd";
-import "antd/dist/antd.css";
+import { Icon, Button, DatePicker } from "antd";
+const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
+
+function onChange(date, dateString) {
+  console.log(date, dateString);
+}
 
 class App extends Component {
   render() {
     return (
       <div style={{ textAlign: "center" }}>
         <h1>Welcome & Login Page</h1>
+        <Icon type="step-backward" />
         <Button type="primary" ghost>
           <a href="/auth/google">LogIn With Google</a>
         </Button>
+        <div>
+          <DatePicker onChange={onChange} />
+          <br />
+          <MonthPicker onChange={onChange} placeholder="Select month" />
+          <br />
+          <RangePicker onChange={onChange} />
+          <br />
+          <WeekPicker onChange={onChange} placeholder="Select week" />
+        </div>
       </div>
     );
   }

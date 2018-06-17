c39943fa76d0cd808ec0405a6558d4f253931166
brianpatrickhummel <frankgucci@mac.com>
2018-06-16 20:40:33 -0700

    heroku build

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -4,7 +4,7 @@
 const { injectBabelPlugin } = require("react-app-rewired");
 const rewireLess = require("react-app-rewire-less");
 
-module.exports = (config, env) => {
+module.exports = function override(config, env) {
   config = injectBabelPlugin(["import", { libraryName: "antd", style: true }], config);
   // change importing css to less
   config = rewireLess.withLoaderOptions({
